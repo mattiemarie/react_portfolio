e82b64c6d4abd6b8126b0755f45a5227e88666c9
Mattie Gonzalez <gonzalem@lssc.edu>
2023-03-05 15:03:48 -0500

    contact me form

---
+++ src/data.js
@@ -0,0 +1,20 @@
+export const projects = [
+    {
+        title: "",
+        subtitle: "Mod 5",
+        image:"",
+        link:"",
+    },
+    {
+        title: "",
+        subtitle: "Mod 9",
+        image:"",
+        link:"",
+    },
+    {
+        title: "",
+        subtitle: "Mod 12",
+        image:"",
+        link:"",
+    },
+];
