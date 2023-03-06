62b528e1c3a04583b9660ed204fea1fbe5af5ccb
Mattie Gonzalez <gonzalem@lssc.edu>
2023-03-06 18:18:41 -0500

    adding to resume.js

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,1 +1,14 @@
-export default function Resume() {}
+import React from "react";
+
+export default function Resume() {
+    return(
+    <section>
+        <div class="heading_container">
+        Resume
+        </div>
+        <div class="container-lg" id="resume_container">
+            INSERT RESUME IMAGE HERE
+        </div>
+    </section>  
+    )
+}
